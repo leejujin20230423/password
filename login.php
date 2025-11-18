c53e33ffe6952d2ae8a69afdb6ba332043d044b0
lokia <lokia@naver.com>
2025-11-19 07:20:52 +0900

    aaa

--- login.php
+++ login.php
@@ -2,6 +2,11 @@
 session_start();
 require_once "db.php";
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 $db = new Database();
 $pdo = $db->connect();
 $error = "";
